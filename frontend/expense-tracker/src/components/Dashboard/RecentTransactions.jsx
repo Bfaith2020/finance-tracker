c4c39f8ce703461b5e2b6ecaf5eeb575daefc638
Bonga2025 <nfmalambe75@gmail.com>
2025-06-23 01:16:08 +0200

    updated imports

--- frontend/expense-tracker/src/components/Dashboard/RecentTransactions.jsx
+++ frontend/expense-tracker/src/components/Dashboard/RecentTransactions.jsx
@@ -3,22 +3,25 @@ import TransactionInfoCard from "../cards/TransactionInfoCard";
 import { LuArrowRight } from "react-icons/lu";
 import moment from "moment";
 
-const RecentTransactions = ({transactions, onSeeMore}) => {
+const RecentTransactions = ({ transactions, onSeeMore }) => {
   return (
     <div className="card">
       <div className="flex items-center justify-between ">
         <h5 className="text-lg ml-3 mt-2">Recent Transactions</h5>
 
-        <button className="card-btn mr-3 mt-1 flex items-center" onClick={onSeeMore}>
+        <button
+          className="card-btn mr-3 mt-1 flex items-center"
+          onClick={onSeeMore}
+        >
           See All <LuArrowRight className="text-base" />
         </button>
       </div>
 
       <div className="mt-6">
-        {transactions?.slice(0,5)?.map((item) => (
+        {transactions?.slice(0, 5)?.map((item) => (
           <TransactionInfoCard
             key={item._id}
-            title={item.type == 'expense' ? item.category : item.source}
+            title={item.type == "expense" ? item.category : item.source}
             icon={item.icon}
             date={moment(item.date).format("Do MMM YYYY")}
             amount={item.amount}
--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState, useMemo } from "react";
 import DashboardLayout from "../../components/Layouts/DashboardLayout";
-
-import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
-import { IoMdCard } from "react-icons/io";
-import { motion } from "framer-motion";
-
-import { useNavigate } from "react-router-dom";
 import InfoCard from "../../components/Cards/InfoCard";
-import { useUserAuth } from "../../hooks/useUserAuth";
-import axiosInstance from "../../utils/axiosInstance";
-import { API_PATHS } from "../../utils/apiPaths";
-import { addThousandsSeparator } from "../../utils/helper";
 import RecentTransactions from "../../components/Dashboard/RecentTransactions";
 import FinanceOverview from "../../components/Dashboard/FinanceOverview";
 import ExpenseTransactions from "../../components/Dashboard/ExpenseTransactions";
 import Last30DaysExpenses from "../../components/Dashboard/Last30DaysExpenses";
 import RecentIncome from "../../components/Dashboard/RecentIncome";
 import RecentIncomeWithChart from "../../components/Dashboard/RecentIncomeWithChart";
+
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+import { IoMdCard } from "react-icons/io";
+import { motion } from "framer-motion";
+
+import { useNavigate } from "react-router-dom";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import { addThousandsSeparator } from "../../utils/helper";
 
 const TIPS_QUOTES = [
   "A budget is like telling your money where to go instead of wondering where it went. – Dave Ramsey",
